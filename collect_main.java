0666a12dc68224216eee29f5dce933033172c6f5
unknown <rlarudgkswkd@gmail.com>
2021-11-15 11:53:24 +0900

    new commit

---
+++ collect_main.java
@@ -0,0 +1,46 @@
+package collect_app;
+
+import java.util.Scanner;
+
+public class collect_main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            int menu_sel = 0;
+            System.out.println();
+            System.out.println("===hwajeong collect program===");
+            System.out.println();
+            System.out.println("1. 인원 등록");
+            System.out.println("2. 인원 조회");
+            System.out.println("3. 인원 삭제");
+            System.out.println("4. 인원 계수");
+            System.out.println("0. 종료");
+            System.out.println();
+            System.out.print("번호 입력 : ");
+            menu_sel = sc.nextInt();
+            switch (menu_sel) {
+                case 1:
+                    System.out.println(menu_sel+"번 선택");
+                    
+                    break;
+                case 2:
+                    System.out.println(menu_sel+"번 선택");
+                    break;
+                case 3:
+                    System.out.println(menu_sel+"번 선택");
+                    break;
+                case 4:
+                    System.out.println(menu_sel+"번 선택");
+                    break;
+                default:
+                    break;
+            }
+            if(menu_sel==0){
+                System.out.println("종료");
+                break;
+            }
+        }
+        sc.close();
+        
+    }
+}
---
+++ menu_class.java
@@ -0,0 +1,8 @@
+package collect_app;
+
+public class menu_class {
+    public static int member_add(){
+        
+        return 0;
+    }
+}
